a6fce8fe48fd2f50718eb8d8c61b4fe86e703db0
geekbrains <info@geebrains.ru>
2022-02-25 14:34:57 +0300

    Initial commit

---
+++ src/Ksenia2/Main.java
@@ -0,0 +1,48 @@
+package Ksenia2;
+
+class Main {
+
+    public static void main(String[] args) {
+        Person[] persArray = new Person[5];
+        persArray[0] = new Person("Василькова П.", "Бугалтер", " vasilcova@mail.com", "89211234576", 40000, 25);
+        persArray[1] = new Person("Петров В.", "Технолог", " petrov@mail.com", "89211234579", 50000, 39);
+        persArray[2] = new Person("Рогов Г.", "Директор", " rogov@mail.com", "89211294576", 80000, 25);
+        persArray[3] = new Person("Паравозов С.", "Менеджер", " paravozov@mail.com", "892112345746", 30000, 30);
+        persArray[4] = new Person("Мулатов Н.", "Отдел кадров", " vmylatov@mail.com", "89211234576", 65000, 55);
+        for (Person person : persArray)
+            if (person.getAge() > 40)
+                System.out.println(person);
+    }
+
+    static class Person {
+        private String name;
+        private String position;
+        private String email;
+        private String phone;
+        private int salary;
+        private int age;
+
+        public Person(String name, String position, String email, String phone, int salary, int age) {
+            this.name = name;
+            this.position = position;
+            this.email = email;
+            this.phone = phone;
+            this.salary = salary;
+            this.age = age;
+        }
+
+        public int getAge() {
+
+            return age;
+        }
+
+        @Override
+        public String toString() {
+            return String.format(name, position, email, phone, salary, age);
+
+        }
+    }
+
+}
+
+
